228374e2f4435ecb92092b7b1c24b98665b4fa7b
Ian Stokes-Rees <ijstokes@continuum.io>
2017-05-18 03:47:38 -0400

    Added R Shiny app

---
+++ normal_distribution/README.md
@@ -0,0 +1,11 @@
+# Simple Shiny App
+
+This simple app demonstrates Anaconda Project being used to
+define an R Shiny app that displays an interactive histogram
+of a random Gaussian data set.
+
+## Start
+
+```
+anaconda-project run
+```
---
+++ normal_distribution/hello/ui.R
@@ -0,0 +1,27 @@
+library(shiny)
+
+# Define UI for application that plots random distributions 
+shinyUI(pageWithSidebar(
+
+  # Application title
+  headerPanel("Hello PyCon!"),
+
+  # Sidebar with a slider input for number of observations
+  sidebarPanel(
+    sliderInput("obs", 
+                "Number of observations:", 
+                min = 10,
+                max = 5000, 
+                value = 500),
+    sliderInput("breaks", 
+                "Bins:", 
+                min = 3,
+                max = 50, 
+                value = 20)
+  ),
+
+  # Show a plot of the generated distribution
+  mainPanel(
+    plotOutput("distPlot")
+  )
+))
